ed33ec43b83e008b15317e83899e2cb6041c163b
Kholkin <kholkins@mail.ru>
2020-04-04 21:40:25 +0300

    add intent

--- app/src/main/java/com/example/sportclub/MainActivity.java
+++ app/src/main/java/com/example/sportclub/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.sportclub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, AddMemberActivity.class);
+                startActivity(intent);
             }
         });
     }
