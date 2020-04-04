f9a8b54beb64c0114879b089b370cc4d6ac36328
Kholkin <kholkins@mail.ru>
2020-04-04 21:33:12 +0300

    add vector image

--- app/src/main/java/com/example/sportclub/MainActivity.java
+++ app/src/main/java/com/example/sportclub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sportclub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
