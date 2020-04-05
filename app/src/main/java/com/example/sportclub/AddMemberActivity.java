6f7543d16e9b6035cf112b10590669edd13e1976
Kholkin <kholkins@mail.ru>
2020-04-05 12:33:11 +0300

    AddMemberActivity

--- app/src/main/java/com/example/sportclub/AddMemberActivity.java
+++ app/src/main/java/com/example/sportclub/AddMemberActivity.java
@@ -3,12 +3,26 @@ package com.example.sportclub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddMemberActivity extends AppCompatActivity {
+
+    private EditText editTextFirstName;
+    private EditText editTextLastName;
+    private EditText editTextGroup;
+    private Spinner spinnerGender;
+    private int gender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextGroup = findViewById(R.id.editTextGroup);
+        spinnerGender = findViewById(R.id.spinnerGender);
+
     }
 }
