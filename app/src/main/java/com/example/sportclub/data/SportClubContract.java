805a6f93e7c5e2c68705f3f0ec3de7175241f428
Kholkin <kholkins@mail.ru>
2020-04-06 16:13:16 +0300

    Edit member menu

--- app/src/main/java/com/example/sportclub/data/SportClubContract.java
+++ app/src/main/java/com/example/sportclub/data/SportClubContract.java
@@ -6,13 +6,16 @@ public final class SportClubContract implements BaseColumns {
     private SportClubContract() {
     }
 
+    public static final int DATABASE_VERSION =1;
+    public static final String DATABASE_NAME = "club";
+
     public final static class MemberEntry{
         public static final String _NAME = "members";
         public static final String COLUMN_ID = BaseColumns._ID;
         public static final String COLUMN_FIRST_NAME = "firstName";
         public static final String COLUMN_LAST_NAME = "lastName";
         public static final String COLUMN_GENDER = "gender";
-        public static final String COLUMN_GROUP = "group";
+        public static final String COLUMN_SPORT = "sport";
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
---
+++ app/src/main/java/com/example/sportclub/data/SportClubOpenHelper.java
@@ -0,0 +1,26 @@
+package com.example.sportclub.data;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SportClubOpenHelper extends SQLiteOpenHelper {
+
+    public SportClubOpenHelper(Context context) {
+        super(context, SportClubContract.DATABASE_NAME, null, SportClubContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
