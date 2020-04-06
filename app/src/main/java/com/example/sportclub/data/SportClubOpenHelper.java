60427dbc8273bda39138ec3467abb8041c077879
Kholkin <kholkins@mail.ru>
2020-04-06 17:03:58 +0300

    add database

--- app/src/main/java/com/example/sportclub/data/SportClubOpenHelper.java
+++ app/src/main/java/com/example/sportclub/data/SportClubOpenHelper.java
@@ -1,12 +1,10 @@
 package com.example.sportclub.data;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import com.example.sportclub.data.SportClubContract.MemberEntry;
 
 public class SportClubOpenHelper extends SQLiteOpenHelper {
 
@@ -16,11 +14,19 @@ public class SportClubOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String CREATE_MEMBERS_TABLE = "CREATE TABLE " + MemberEntry._NAME + "("
+                + MemberEntry.COLUMN_ID + " INTEGER PRIMARY KEY,"
+                + MemberEntry.COLUMN_FIRST_NAME + " TEXT,"
+                + MemberEntry.COLUMN_LAST_NAME + " TEXT,"
+                + MemberEntry.COLUMN_GENDER + " INTEGER NOT NULL,"
+                + MemberEntry.COLUMN_SPORT + " TEXT" + ")";
 
+        db.execSQL(CREATE_MEMBERS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + MemberEntry._NAME);
+        onCreate(db);
     }
 }
