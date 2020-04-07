e6dafce7adb100099b9e16fb80932b6ef71e9dd4
Kholkin <kholkins@mail.ru>
2020-04-07 15:17:16 +0300

    display data

--- app/src/main/java/com/example/sportclub/MainActivity.java
+++ app/src/main/java/com/example/sportclub/MainActivity.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.sax.StartElementListener;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import com.example.sportclub.data.SportClubContract.MemberEntry;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewData = findViewById(R.id.textViewData);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +31,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void displayData(){
+        String[] projection = {
+                MemberEntry.COLUMN_ID,
+                MemberEntry.COLUMN_FIRST_NAME,
+                MemberEntry.COLUMN_LAST_NAME,
+                MemberEntry.COLUMN_GENDER,
+                MemberEntry.COLUMN_SPORT
+        };
+        getContentResolver().query(MemberEntry.CONTENT_URI,projection,null ,null,null);
     }
 }
